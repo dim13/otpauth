bfa71ff47261ebcf48b18a93f026fd00c778c4c1
Dimitri Sokolyuk <demon@dim13.org>
2020-06-23 08:12:24 +0200

    cleanup

--- migration/convert.go
+++ migration/convert.go
@@ -18,17 +18,17 @@ var (
 )
 
 var (
-	otpTypes = map[Payload_OtpType]string{
+	typeString = map[Payload_OtpType]string{
 		Payload_OTP_TYPE_HOTP: "hotp",
 		Payload_OTP_TYPE_TOTP: "totp",
 	}
-	algorithms = map[Payload_Algorithm]string{
+	algString = map[Payload_Algorithm]string{
 		Payload_ALGORITHM_SHA1:   "SHA1",
 		Payload_ALGORITHM_SHA256: "SHA256",
 		Payload_ALGORITHM_SHA512: "SHA512",
 		Payload_ALGORITHM_MD5:    "MD5",
 	}
-	digitCounts = map[Payload_DigitCount]string{
+	digitsString = map[Payload_DigitCount]string{
 		Payload_DIGIT_COUNT_SIX:   "6",
 		Payload_DIGIT_COUNT_EIGHT: "8",
 	}
@@ -46,11 +46,11 @@ func (op *Payload_OtpParameters) URL() *url.URL {
 	}
 	// optional
 	if op.Algorithm != Payload_ALGORITHM_UNSPECIFIED {
-		v.Add("algorithm", algorithms[op.Algorithm])
+		v.Add("algorithm", algString[op.Algorithm])
 	}
 	// optional
 	if op.Digits != Payload_DIGIT_COUNT_UNSPECIFIED {
-		v.Add("digits", digitCounts[op.Digits])
+		v.Add("digits", digitsString[op.Digits])
 	}
 	// required if type is hotp
 	if op.Type == Payload_OTP_TYPE_HOTP {
@@ -62,7 +62,7 @@ func (op *Payload_OtpParameters) URL() *url.URL {
 	}
 	return &url.URL{
 		Scheme:   "otpauth",
-		Host:     otpTypes[op.Type],
+		Host:     typeString[op.Type],
 		Path:     op.Name,
 		RawQuery: v.Encode(),
 	}
--- migration/evaluate.go
+++ migration/evaluate.go
@@ -16,19 +16,19 @@ import (
 )
 
 var (
-	hashes = map[Payload_Algorithm]func() hash.Hash{
+	hashFunc = map[Payload_Algorithm]func() hash.Hash{
 		Payload_ALGORITHM_UNSPECIFIED: sha1.New, // default
 		Payload_ALGORITHM_SHA1:        sha1.New,
 		Payload_ALGORITHM_SHA256:      sha256.New,
 		Payload_ALGORITHM_SHA512:      sha512.New,
 		Payload_ALGORITHM_MD5:         md5.New,
 	}
-	digits = map[Payload_DigitCount]int{
+	digitCount = map[Payload_DigitCount]int{
 		Payload_DIGIT_COUNT_UNSPECIFIED: 1e6, // default
 		Payload_DIGIT_COUNT_SIX:         1e6,
 		Payload_DIGIT_COUNT_EIGHT:       1e8,
 	}
-	counter = map[Payload_OtpType]func(*Payload_OtpParameters) int64{
+	countFunc = map[Payload_OtpType]func(*Payload_OtpParameters) int64{
 		Payload_OTP_TYPE_UNSPECIFIED: otpTOTP, // default
 		Payload_OTP_TYPE_HOTP:        otpHOTP,
 		Payload_OTP_TYPE_TOTP:        otpTOTP,
@@ -43,12 +43,12 @@ func otpTOTP(op *Payload_OtpParameters) int64 { return now().Unix() / 30 }
 
 // Evaluate OTP parameters
 func (op *Payload_OtpParameters) Evaluate() int {
-	h := hmac.New(hashes[op.Algorithm], op.Secret)
-	binary.Write(h, binary.BigEndian, counter[op.Type](op))
-	hash := h.Sum(nil)
-	off := hash[h.Size()-1] & 15
-	header := binary.BigEndian.Uint32(hash[off:]) & (1<<31 - 1)
-	return int(header) % digits[op.Digits]
+	h := hmac.New(hashFunc[op.Algorithm], op.Secret)
+	binary.Write(h, binary.BigEndian, countFunc[op.Type](op))
+	hashed := h.Sum(nil)
+	offset := hashed[h.Size()-1] & 15
+	result := binary.BigEndian.Uint32(hashed[offset:]) & (1<<31 - 1)
+	return int(result) % digitCount[op.Digits]
 }
 
 // Evaluate otpauth-migration URL
