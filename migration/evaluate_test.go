4dea36a6dfe72f15fa0dfb8428a07180ea0db600
Dimitri Sokolyuk <demon@dim13.org>
2020-08-16 19:53:41 +0200

    convert to test

--- migration/evaluate_test.go
+++ migration/evaluate_test.go
@@ -1,26 +1,27 @@
 package migration
 
 import (
-	"fmt"
-	"log"
 	"net/url"
+	"testing"
 	"time"
 )
 
-func ExampleEvaluate() {
+func TestEvaluate(t *testing.T) {
 	// fake time
 	now = func() time.Time { return time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC) }
 	u, err := url.Parse("otpauth-migration://offline?data=CjEKCkhlbGxvId6tvu8SGEV4YW1wbGU6YWxpY2VAZ29vZ2xlLmNvbRoHRXhhbXBsZTAC")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	p, err := Unmarshal(u)
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
-	for _, op := range p.OtpParameters {
-		fmt.Printf("%06d %s\n", op.Evaluate(), op.Name)
+	if len(p.OtpParameters) < 1 {
+		t.Fatalf("got lengh %v, want 1", len(p.OtpParameters))
 	}
-	// Output:
-	// 528064 Example:alice@google.com
+	res := p.OtpParameters[0].Evaluate()
+	if res != 528064 {
+		t.Errorf("got %v", res)
+	}
 }
